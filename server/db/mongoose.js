cac66accfab0bdccf179ec9caaf3e640ef303012
Bidhan Bhandari <bhandariprachhanna@gmail.com>
2019-02-24 14:22:30 -0600

    Made node version change on engine inside package.json

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+//process.env.MONGODB_URI is the URI for Mlab integrated with heroku
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
